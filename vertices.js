0be8cff9fb8b878ccd1e7582ce6c0785cdc464ba
MDN <mark.denardo@gmail.com>
2023-02-20 17:24:45 -0500

    Update vertices.js

--- vertices.js
+++ vertices.js
@@ -1,8 +1,6 @@
-#please provide the p5.js code for a geometric shape which changes the number of vertices based on the mouse x,y position
-
-#chatGPT response:
-
-#Sure, here's an example p5.js code that creates a geometric shape with a variable number of vertices based on the position of the mouse:
+//please provide the p5.js code for a geometric shape which changes the number of vertices based on the mouse x,y position
+//chatGPT response:
+//Sure, here's an example p5.js code that creates a geometric shape with a variable number of vertices based on the position of the mouse:
 
 let numVertices = 3; // starting number of vertices
 let radius = 100; // radius of the shape
@@ -26,3 +24,10 @@ function draw() {
   }
   endShape(CLOSE);
 }
+
+//This code sets the initial number of vertices to 3 and the radius of the shape to 100. In the draw() function, 
+// it maps the x-position of the mouse to a range of 3 to 12, which becomes the new number of vertices. 
+// It then calculates the angle between each vertex based on the number of vertices, 
+//and uses this angle to calculate the x and y coordinates of each vertex on the circle. 
+//Finally, it draws the shape using the beginShape() and endShape() functions. 
+//The CLOSE argument to endShape() ensures that the shape is closed.
